4ae3ee12c8dedb3b9b35706c9f43652f486e5bcc
Lorenzo <lorenzo.aversano@edu.devinci.fr>
2023-10-16 13:07:42 +0200

    Finalisation de l'inscription. Fix #2

--- validation.php
+++ validation.php
@@ -5,9 +5,21 @@ session_start();
 
 if(	isset($_POST['username']) && isset($_POST['email']) && isset($_POST['password']) && 
 	!empty($_POST['username']) && !empty($_POST['email']) && !empty($_POST['password'])) {
-
-	// TODO
-
+	$username = $_POST['username'];
+	$email = $_POST['email'];
+	$password = $_POST['password'];
+	if(isEmailAvailable($db, $email)){
+		if(isUsernameAvailable($db, $username)){
+			userRegistration($db, $username, $email, $password);
+			header('Location: login.php');
+		}else{
+			$_SESSION['message'] = 'Le nom d\'utilisateur est déjà utilisé.';
+			header('Location: register.php');
+		}
+	}else{
+		$_SESSION['message'] = 'L\'email est déjà associé à un compte.';
+		header('Location: register.php');
+	}
 }else{ 
 	$_SESSION['message'] = 'Erreur : Formulaire incomplet';
 	header('Location: register.php');
